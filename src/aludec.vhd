1b8b5092cf8feb0332e676c3af4202e85cef4d57
rTaiShan <raul.tai@hotmail.com>
2023-11-28 12:09:55 -0300

    Reformat aludec process

--- src/aludec.vhd
+++ src/aludec.vhd
@@ -14,26 +14,28 @@ begin
     RtypeSub <= funct7b5 and opb5;
     -- TRUE for R–type subtract
     process (opb5, funct3, funct7b5, ALUOp, RtypeSub) begin
-        case ALUOp is when "00" => ALUControl <= "000";
-                -- addition
-            when "01" => ALUControl <= "001";
-                -- subtraction
-            when others => case funct3 is -- R–type or I–type ALU
-            when "000" = if RtypeSub = '1' then
-                ALUControl <= "001";
-                -- sub
-            else
-                ALUControl <= "000";
-                -- add, addi
-        end if;
-        when "010" => ALUControl <= "101";
-        -- slt, slti
-        when "110" => ALUControl <= "011";
-        -- or, ori
-        when "111" => ALUControl <= "010";
-        -- and, andi
-        when others => ALUControl <= "---";
-        -- unknown
-    end case;
-end case;
-end process;
+        case ALUOp is 
+            when "00" => 
+                ALUControl <= "000"; -- addition
+            when "01" => 
+                ALUControl <= "001"; -- subtraction
+            when others => 
+                case funct3 is -- R–type or I–type ALU
+                    when "000" =>
+                        if RtypeSub = '1' then
+                            ALUControl <= "001"; -- sub
+                        else 
+                            ALUControl <= "000"; -- add, addi
+                        end if;
+                    when "010" => 
+                        ALUControl <= "101"; -- slt, slti
+                    when "110" => 
+                        ALUControl <= "011"; -- or, ori
+                    when "111" => 
+                        ALUControl <= "010"; -- and, andi
+                    when others => 
+                        ALUControl <= "---"; -- unknown
+                end case;
+        end case;
+    end process;
+end;

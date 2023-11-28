3916707a65d1dfb975fab1d0255ed3fb5550e66f
rTaiShan <raul.tai@hotmail.com>
2023-11-28 12:12:10 -0300

    Renamed muxes to avoid clash

--- src/mux3x1.vhd
+++ src/mux3x1.vhd
@@ -1,27 +1,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
-entity mux2 is
-    generic (width : integer := 8);
-    port (
-        d0, d1 : in std_logic_vector(width-1 downto 0);
-        s : in std_logic;
-        y : out std_logic_vector(width-1 downto 0));
-end;
-architecture behave of mux2 is
-begin
-    y <= d1 when s = '1' else
-        d0;
-end;
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
-entity mux3 is
+entity mux3x1 is
     generic (width : integer := 8);
     port (
         d0, d1, d2 : in std_logic_vector(width-1 downto 0);
         s : in std_logic_vector(1 downto 0);
         y : out std_logic_vector(width-1 downto 0));
 end;
-architecture behave of mux3 is
+architecture behave of mux3x1 is
 begin
     process (d0, d1, d2, s) begin
         if (s = "00") then
